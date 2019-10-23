190ccc07a417d3d2bbbebc6512ace7d8dcca3774
heyjul3s <julian.cy.low@gmail.com>
2019-10-23 22:27:08 +1100

    remove docgen config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
 
 module.exports = ({ config }) => {
   config.resolve.modules = [
@@ -15,12 +14,10 @@ module.exports = ({ config }) => {
       'babel-loader',
       require.resolve('@storybook/addon-storysource/loader'),
       require.resolve('awesome-typescript-loader'),
-      require.resolve('react-docgen-typescript-loader'),
     ],
     enforce: 'pre',
   });
 
-  config.plugins.push(new TSDocgenPlugin());
   config.resolve.extensions.push('.ts', '.tsx');
 
   return config;
